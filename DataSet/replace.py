6d2bcfd6799ae4ee4ae3c3af552b5c727ab760fe
Marten de Jong <marten.de.jong@student.nhlstenden.com>
2023-02-02 13:43:41 +0100

    seperated data

--- DataSet/replace.py
+++ DataSet/replace.py
@@ -1,8 +1,8 @@
-with open('imdb-urls.txt', 'r') as file :
+with open('movie urls.txt', 'r') as file :
 	filedata = file.read();
 
-
 filedata = filedata.replace('tt', 'https://m.imdb.com/title/tt')
+#filedata = filedata+s
 
-with open('imdb-urls.txt', 'w') as file :
+with open('movie urls.txt', 'w') as file :
 	file.write(filedata)
