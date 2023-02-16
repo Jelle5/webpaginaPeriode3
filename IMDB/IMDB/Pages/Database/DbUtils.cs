f00a81e9ffa39a4d209f8f04987d063c92398ad7
Jessedekeizer <jessedekeizer03@gmail.com>
2023-02-16 11:25:25 +0100

    db utils

---
+++ IMDB/IMDB/Pages/Database/DbUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace WebApplication2.Pages.Database
+{
+    public class DbUtils
+    {   
+        public IDbConnection Connect()
+        {
+            var connString = "Host=82.73.46.252:5432;Username=Website;Password=IMDB-Website-Groep10;Database=imdb";
+
+            return new NpgsqlConnection(connString);
+            
+        }
+    }
+}
