0925159849cc884412e927808a7d92a258260313
Jelle5 <jelle.van.der.ploeg@student.nhlstenden.com>
2023-02-17 11:22:37 +0100

    models

--- IMDB/IMDB/Pages/DashBoard.cshtml.cs
+++ IMDB/IMDB/Pages/DashBoard.cshtml.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication2.Pages.Database.model;
+
 
 namespace WebApplication2.Pages;
 
 public class DashBoard : PageModel
 {
-
     public void OnGet()
     {
         
---
+++ IMDB/IMDB/Pages/Database/model/Title.cs
@@ -0,0 +1,28 @@
+namespace WebApplication2.Pages.Database.model;
+
+public class Title
+{
+    public string tconst { get; set; }
+    
+    public string type { get; set; }
+    
+    public string primary { get; set; }
+    
+    public int isadult { get; set; }
+    
+    public int startyear { get; set; }
+    
+    public int endyear { get; set; }
+    
+    public int runtimemin { get; set; }
+
+    public double averagerating { get; set; }
+     
+    public int numvotes { get; set; }
+    
+    public int seasonnr { get; set; }
+    
+    public int episodenr { get; set; }
+    
+    public string parent { get; set; }
+}
