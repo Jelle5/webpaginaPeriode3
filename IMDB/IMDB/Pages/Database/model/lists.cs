202b724b76ac038c539101df29f42a98b01ae73c
Jelle5 <91126597+Jelle5@users.noreply.github.com>
2023-03-06 13:27:56 +0100

    woorden lijst

---
+++ IMDB/IMDB/Pages/Database/model/lists.cs
@@ -0,0 +1,11 @@
+namespace WebApplication2.Pages.Database.model;
+
+public class lists
+{
+    public List<Tuple<string, string>> Title2 = new List<Tuple<string, string>>
+    {
+        new Tuple<string, string>( "asdf", "asdf"),
+        
+    };
+
+}
