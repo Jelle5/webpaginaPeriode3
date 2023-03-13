45cd3e7020369e2f74bbcd76bf13248239be34a7
Jelle5 <91126597+Jelle5@users.noreply.github.com>
2023-03-13 12:15:37 +0100

    woordenboek vergroot

--- IMDB/IMDB/Pages/Database/model/lists.cs
+++ IMDB/IMDB/Pages/Database/model/lists.cs
@@ -35,6 +35,7 @@ public class lists
     public List<Tuple<string, string>> individual = new List<Tuple<string, string>>
     {
         new Tuple<string, string>( "naam", "primary"),
+        new Tuple<string, string>( "namen", "primary"),
         new Tuple<string, string>( "actrice", "primary"),
         new Tuple<string, string>( "actrices", "primary"),
         new Tuple<string, string>( "acteur", "primary"),
@@ -44,6 +45,7 @@ public class lists
         new Tuple<string, string>( "geboorte", "birthyear"),
         new Tuple<string, string>( "geboren", "birthyear"),
         new Tuple<string, string>( "sterf", "deathyear"),
+        new Tuple<string, string>( "sterfjaar", "deathyear"),
         new Tuple<string, string>( "overleden", "deathyear"),
         new Tuple<string, string>( "dood", "deathyear")
         
@@ -51,7 +53,8 @@ public class lists
 
     public List<Tuple<string, string>> genre = new List<Tuple<string, string>>
     {
-        new Tuple<string, string>( "genre", "genre")
+        new Tuple<string, string>( "genre", "genre"),
+        new Tuple<string, string>( "genres", "genre")
     };
 
     public List<Tuple<string, string>> profession = new List<Tuple<string, string>>
@@ -84,19 +87,22 @@ public class lists
     
     public List<Tuple<string, string>> color = new List<Tuple<string, string>>
     {
-        new Tuple<string, string>( "kleur", "name")
+        new Tuple<string, string>( "kleur", "name"),
+        new Tuple<string, string>( "gekleurd", "name")
     };
     
     public List<Tuple<string, string>> site = new List<Tuple<string, string>>
     {
         new Tuple<string, string>( "webpagina", "site"),
         new Tuple<string, string>( "site", "site"),
+        new Tuple<string, string>( "website", "site"),
         new Tuple<string, string>( "link", "link")
     };
     
     public List<Tuple<string, string>> aka = new List<Tuple<string, string>>
     {
         new Tuple<string, string>( "titel", "title"),
+        new Tuple<string, string>( "titels", "title"),
         new Tuple<string, string>( "regio", "region"),
         new Tuple<string, string>( "taal", "language"),
         new Tuple<string, string>( "hoofdtitel", "isoriginal"),
@@ -107,11 +113,18 @@ public class lists
     {
         new Tuple<string, string>("gemiddeld", ""),
         new Tuple<string, string>("gemiddelde", ""),
+        new Tuple<string, string>("laag", "<"),
+        new Tuple<string, string>("laagste", "<"),
         new Tuple<string, string>("hoger", ">"),
+        new Tuple<string, string>("hogere", ">"),
         new Tuple<string, string>("groter", ">"),
+        new Tuple<string, string>("grotere", ">"),
         new Tuple<string, string>("kleiner", "<"),
+        new Tuple<string, string>("kleinere", "<"),
         new Tuple<string, string>("minder", "<"),
         new Tuple<string, string>("gelijk", "="),
+        new Tuple<string, string>("gelijke", "="),
+        new Tuple<string, string>("gelijken", "="),
         new Tuple<string, string>("is", "="),
     };
 
