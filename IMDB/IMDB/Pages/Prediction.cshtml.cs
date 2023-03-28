3d17f78a968707d841acfd76099653dd154f4151
Jelle5 <91126597+Jelle5@users.noreply.github.com>
2023-03-28 11:48:34 +0200

    image

--- IMDB/IMDB/Pages/Prediction.cshtml.cs
+++ IMDB/IMDB/Pages/Prediction.cshtml.cs
@@ -7,31 +7,16 @@ namespace WebApplication2.Pages;
 
 public class Prediction : PageModel
 {
+    public static string relativePath = @"Images/Rpred.png";
+
+    public static string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+    public string fullPath = Path.Combine(baseDirectory, relativePath);
+    
+    
+
     public void OnGet()
     {
-        var data = model();
-
-        double[] y = { };
-        double[][] x = { };
-
-        var regression = MultipleRegression.NormalEquations(x, y);
-        
-
-    }
-
-    public IEnumerable<IEnumerable<string>> model()
-    {
-        var lines = System.IO.File.ReadAllLines("csvData/DashboardTable.csv");
-
-        var header = lines[0].Split(';');
-
-        var columnIndex = Array.IndexOf(header, "column_name");
-
-        var columnIndices = header.Select((name, index) => new { Name = name, Index = index })
-            .Where(column => new[] { "tconst", "type", "isadult", "startyear", "endyear", "runtimemin", "averagerating", "numvotes", "seasonnr", "episodenr", "parent", "budget", "gross_domestic", "gross_worldwide", "opening_weekend", "origin", "filming_country", "aspect_ratio" }.Contains(column.Name))
-            .Select(column => column.Index);
-
-        var columnData = lines.Skip(1).Select(line => columnIndices.Select(index => line.Split(';')[index]));
-        return columnData;
+        //PREDICT.Attribute["src"] = fullPath;
     }
 }
