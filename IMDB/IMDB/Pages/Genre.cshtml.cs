86a70ce35c33403007046161353485763bba1a7d
Marten de Jong <marten.de.jong@student.nhlstenden.com>
2023-03-30 10:54:30 +0200

    Update Genre.cshtml.cs

--- IMDB/IMDB/Pages/Genre.cshtml.cs
+++ IMDB/IMDB/Pages/Genre.cshtml.cs
@@ -1,31 +1,51 @@
 
+using System.Data;
 using System.Text.Json;
+using Dapper;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication2.Pages.Database;
+using WebApplication2.Pages.Database.model;
 
 namespace WebApplication2.Pages;
 
 public class Genre : PageModel
 {
     public string[] x { get; set; }
-    public double[] y { get; set; }
+    public decimal[] y { get; set; }
     public int[] size { get; set; }
     
     public void OnGet()
     {
-        // Load your data here
-        string json = System.IO.File.ReadAllText("GraphData/avgRatingCountGenre.json");
-        var data = JsonSerializer.Deserialize<List<MyData>>(json);
+        IEnumerable<avgCountGenre> avgCountGenres = getavgCountGenre();
+        x = avgCountGenres.Select(x => x.genre).ToArray();
+        y = avgCountGenres.Select(x => Math.Round(x.averagerating, 1)).ToArray();
+        size = avgCountGenres.Select(x => x.count / 25000).ToArray();
+    }
+    
+    private IDbConnection getConnection()
+    {
+        return new DbUtils().Connect();
+    }
+    
+    public IEnumerable<avgCountGenre> getavgCountGenre()
+    {
+        string sql = @"SELECT genre, count(t.tconst), CAST(avg(averagerating) AS DECIMAL(10,2)) AS averagerating
+                    FROM title t
+                    INNER JOIN genres gs ON gs.tconst = t.tconst
+                    INNER JOIN genre g ON gs.gconst = g.gconst
+                    GROUP BY genre";
 
-        // Convert to arrays
-        x = data.Select(x => x.x).ToArray();
-        y = data.Select(x => Math.Round(x.y, 1)).ToArray();
-        size = data.Select(x => x.r / 25000).ToArray();
+        using var connection = getConnection();
+        var avgCountGenre = connection.Query<avgCountGenre>(sql);
+
+        return avgCountGenre;
     }
 }
 
-public class MyData
+public class avgCountGenre
 {
-    public string x { get; set; }
-    public double y { get; set; }
-    public int r { get; set; }
+    public string genre { get; set; }
+    public int count { get; set; }
+    public decimal averagerating { get; set; }
 }
+
